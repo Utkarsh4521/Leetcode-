1827c924938272bd5880cb3fde7b6d314f6ab71b
utkarsh agarwal <utkarsh.academics1sr01@gmail.com>
2026-03-12 14:40:28 +0530

    Time: 2016 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -3,3 +3,4 @@ select e.employee_id , e.name,count(m.reports_to) as reports_count,round(AVG(m.a
 from Employees e join Employees m
 on e.employee_id = m.reports_to
 group by m.reports_to
+order by e.employee_id
