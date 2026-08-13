e4ef29147e95e947e2257f88dbd522bbfa42fbae
utkarsh agarwal <utkarsh.academics1sr01@gmail.com>
2026-08-13 23:50:24 +0530

    Time: 2 ms (99.64%), Space: 44.6 MB (60.6%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,11 +2,18 @@ import java.util.Arrays;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a, b);
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()){
+            freq[c-'a']++;
+        }
+        for(char c : t.toCharArray()){
+            freq[c-'a']--;
+        }
+        for(int x : freq){
+            if(x!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
