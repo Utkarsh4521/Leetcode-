ccc506c6d9176983ff54d29bf42bf4d45a551732
utkarsh agarwal <utkarsh.academics1sr01@gmail.com>
2026-08-19 22:26:20 +0530

    Time: 17 ms (13.02%), Space: 46.9 MB (39.84%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i =0;i<magazine.length();i++){
+            map.put(magazine.charAt(i),map.getOrDefault(magazine.charAt(i),0)+1);
+        }
+        for(char c : ransomNote.toCharArray()){
+            if(!map.containsKey(c)|| map.get(c)==0){
+                return false;
+            }
+            else{
+                map.put(c,map.get(c)-1);
+            }
+        }
+    return true;
+    }
+}
